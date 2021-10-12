2b9171c6d7f68bb71eedcdf52f35b135ab917d18
Yuvraj Dagur <yuvraj.dagur928@gmail.com>
2021-10-13 03:26:23 +0530

    deployment changes

--- index.js
+++ index.js
@@ -7,30 +7,30 @@ const csv = require("csvtojson");
 const filePath = "data/data.csv";
 
 app.get('/', (req, res) => {
-        var ranks = {
-            "gold": [],
-            "silver": [],
-            "bronze": [],
-            "unranked": []
-        };
-        csv().fromFile(filePath).then((jsonObj) => {
-            jsonObj.forEach(person => {
-                delete person['Student Email'];
-                var numTrack1Badges = parseInt(person['# of Skill Badges Completed in Track 1']);
-                var numTrack2Badges = parseInt(person['# of Skill Badges Completed in Track 2']);
-                if (numTrack1Badges == 6 && numTrack2Badges == 6) {
-                    ranks.gold.push(person);
-                } else if ((numTrack1Badges == 6 && numTrack2Badges != 6) || (numTrack1Badges != 6 && numTrack2Badges == 6)) {
-                    ranks.silver.push(person);
-                } else if ((numTrack1Badges != 6 && numTrack1Badges != 0) && (numTrack2Badges != 6 && numTrack2Badges != 0)) {
-                    ranks.bronze.push(person);
-                } else {
-                    ranks.unranked.push(person)
-                }
-            })
-            res.send({ ranks, goldLen: ranks.gold.length, silverLen: ranks.silver.length, bronzeLen: ranks.bronze.length, unrankedLen: ranks.unranked.length });
-        });
-    })
-    // Listen to Server
+    var ranks = {
+        "gold": [],
+        "silver": [],
+        "bronze": [],
+        "unranked": []
+    };
+    csv().fromFile(filePath).then((jsonObj) => {
+        jsonObj.forEach(person => {
+            delete person['Student Email'];
+            var numTrack1Badges = parseInt(person['# of Skill Badges Completed in Track 1']);
+            var numTrack2Badges = parseInt(person['# of Skill Badges Completed in Track 2']);
+            if (numTrack1Badges == 6 && numTrack2Badges == 6) {
+                ranks.gold.push(person);
+            } else if ((numTrack1Badges == 6 && numTrack2Badges != 6) || (numTrack1Badges != 6 && numTrack2Badges == 6)) {
+                ranks.silver.push(person);
+            } else if ((numTrack1Badges != 6 && numTrack1Badges != 0) && (numTrack2Badges != 6 && numTrack2Badges != 0)) {
+                ranks.bronze.push(person);
+            } else {
+                ranks.unranked.push(person)
+            }
+        })
+        res.send({ ranks, goldLen: ranks.gold.length, silverLen: ranks.silver.length, bronzeLen: ranks.bronze.length, unrankedLen: ranks.unranked.length });
+    });
+})
+// Listen to Server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`server on ${PORT}...`));
